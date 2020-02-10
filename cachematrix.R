b0ca38d8cb9eafae5493fc5e6220f772ba261be8
lcarato <lucianocaratori@gmail.com>
2020-02-10 00:52:02 -0300

    My attempt to solve the problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function inverses a matrix and stores its inverse matrix in cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversa<-solve(x)
+        already_done<<-x
+        cached <<- inversa 
+        print('Cached the inverse matrix of ')
+        print(x)
+        print("as :")
+        print(cached)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the matrix to be solved is already in cache. If it is, it gets the stored value. In the contrary, solves it and returns the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        already<<-already_done
+        ifelse(x==already,inversa<<-cached,inversa<-solve(x))
+        makeCacheMatrix(x)
+        return(inversa)
 }
